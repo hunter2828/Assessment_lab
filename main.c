2547d2c2fed4de7e6a3ebc1bee9de3bf58cd11d8
Hunter Bake-Smith <hunter.bakesmith@gmail.com>
2019-04-03 00:01:11 +0000

    Code made from my pseudocode, not sure if it works yet

--- main.c
+++ main.c
@@ -2,11 +2,40 @@
 
 int main()
 {
-    asdf
-    asdf
-    asdf
-    asdf
-    asdf
-    asdf
+int key, i;// Declared as integers as the key must be an integer and i will be incremented in a loop.
+char message[100], ch; //message[100] array allows a message of 100 characters
+printf("Enter a message to encrypt: ");// User enters a message which is to be encrypted, and it is stored as a type char, called message
+scanf("%c", message);// text stored as variable type char, named message
+
+    for (i = 0; message[i] != 0; ++i)//for loop used because it reduces the amount of code that needs to be written, and allows the whole string to be tested for in one loop
+    {
+        ch = message[i];
+        
+        if(ch>= 'a' && ch<= 'z')//if loop to encrypt lower case letters. The loop adds the key to the value, and if the value exceeds the value of lowercase 'z', it initiates another if loop.
+        {
+            ch = ch + key;//a letters ascii code plus the key is assigned to the same variable butwill have a different ascii code
+            if(ch > 'z')//used to encrypt an values that 'overflow' past the ascii value of 'z'
+            {
+                ch = ch - 26;//formula encrypts values that overflow past z, by using previous value calculated for the new 'ch' and minuses 26.
+            }
+            
+            message[i] = ch;
+        }
+        
+        else(ch>= 'A' && ch<= 'Z')//if loop to encrypt lower case letters. The loop adds the key to the value, and if the value exceeds the value of uppercase 'Z' it initiates another loop
+        {
+            ch = ch + key;//a letters ascii code plus the key is assigned to the same variable butwill have a different ascii code
+            if(ch > 'Z')//used to encrypt an values that 'overflow' past the ascii value of 'Z'
+            {
+                ch = ch - 26;
+            }
+            
+            message[i] = ch;
+        }
+        
+    }
+    printf("Encrypted Message: %s", message);
+
+
   return 0;
 }
